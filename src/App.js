2d796ca8a18617a502a4550c26933f704fb54267
NarmadhaDevarajan <narmi.jan4@gmail.com>
2023-06-09 21:47:26 +0530

    project

---
+++ src/App.js
@@ -0,0 +1,20 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  //console.log("hello from component");
+  const now=new Data();
+  const a=10;
+  const b=20;
+  console.log(now, a+b);
+
+  return (
+    <div>
+  <p>Hello World!</p>
+</div>    
+  
+    );
+}
+
+export default App;
+
