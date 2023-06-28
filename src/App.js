141b7422ef7f33a98d55aa08cc8542508fdd95b6
NarmadhaDevarajan <narmi.jan4@gmail.com>
2023-06-28 18:53:36 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,175 +1,133 @@
-// import 'bootstrap/dist/css/bootstrap.css';
-// import 'bootstrap/dist/js/bootstrap.bundle';
-// import './App.css';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faFolder, faLaughWink } from '@fortawesome/free-regular-svg-icons';
-// import { faChartArea, faCog, faTable, faTachometerAlt, faWrench } from '@fortawesome/free-solid-svg-icons';
+// A Simple Note taking Application
+import axios from 'axios';
+import React, { useEffect, useRef, useState } from 'react';
 
-// function App() {
-//   return (
-//     <div id='wrapper'>
-//         <ul className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
-//             <a className="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-//                 <div className="sidebar-brand-icon rotate-n-15">
-//             <i className="fas fa-laugh-wink"></i>
-//             <FontAwesomeIcon icon={faLaughWink}/>
-//                 </div>
-//                 <div className="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
-//             </a>
-//             <hr className="sidebar-divider my-0"></hr>
+function Note({note}) {
+  return (
+    <li>{ note.content }</li>
+  )
+}
 
-//             <li className="nav-item active">
-//                 <a className="nav-link" href="index.html">
-//             <FontAwesomeIcon icon={faTachometerAlt} />
-//                     <span>Dashboard</span></a>
-//             </li>
+function App(props) {
+  
+  // define a state
+  const [notes, setNotes] = useState([]);
+  const [newNoteContent, setNewNoteContent] = useState('');
+  const [newNoteImportant, setNewNoteImportant] = useState('');
+  const [showStatus, setShowStatus] = useState('all');
 
-//             <hr className="sidebar-divider"></hr>
+  // get the data
+  useEffect(() => {
+    axios
+    .get('http://localhost:3000')
+    .then(response=>setNotes(response.data));
+    // setNotes(props.notes);
+  }, []);
 
-//             <div className="sidebar-heading">
-//                 Interface
-//             </div>
-
-//             <li className="nav-item">
-//                 <a className="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
-//                     aria-expanded="true" aria-controls="collapseTwo">
-//             <FontAwesomeIcon icon={faCog}/>
-//                     <span>Components</span>
-//                 </a>
-//                 <div id="collapseTwo"  className="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-//                     <div className="bg-white py-2 collapse-inner rounded">
-//                         <h6 className="collapse-header">Custom Components:</h6>
-//                         <a className="collapse-item" href="buttons.html">Buttons</a>
-//                         <a className="collapse-item" href="cards.html">Cards</a>
-//                     </div>
-//                 </div>
-//             </li>
-
-//             <li className="nav-item">
-//                 <a className="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
-//                     aria-expanded="true" aria-controls="collapseUtilities">
-//             <FontAwesomeIcon icon={faWrench}/>
-//                     <span>Utilities</span>
-//                 </a>
-//                 <div id="collapseUtilities" className="collapse" aria-labelledby="headingUtilities"
-//                     data-parent="#accordionSidebar">
-//                     <div className="bg-white py-2 collapse-inner rounded">
-//                         <h6 className="collapse-header">Custom Utilities:</h6>
-//                         <a className="collapse-item" href="utilities-color.html">Colors</a>
-//                         <a className="collapse-item" href="utilities-border.html">Borders</a>
-//                         <a className="collapse-item" href="utilities-animation.html">Animations</a>
-//                         <a className="collapse-item" href="utilities-other.html">Other</a>
-//                     </div>
-//                 </div>
-//             </li>
-
-//             <hr className="sidebar-divider"></hr>
-
-//             <div className="sidebar-heading">
-//                 Addons
-//             </div>
-
-//             <li className="nav-item">
-//                 <a className="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
-//                     aria-expanded="true" aria-controls="collapsePages">
-//             <FontAwesomeIcon icon={faFolder} />
-//                     <span>Pages</span>
-//                 </a>
-//                 <div id="collapsePages" className="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-//                     <div className="bg-white py-2 collapse-inner rounded">
-//                         <h6 className="collapse-header">Login Screens:</h6>
-//                         <a className="collapse-item" href="login.html">Login</a>
-//                         <a className="collapse-item" href="register.html">Register</a>
-//                         <a className="collapse-item" href="forgot-password.html">Forgot Password</a>
-//                         <div className="collapse-divider"></div>
-//                         <h6 className="collapse-header">Other Pages:</h6>
-//                         <a className="collapse-item" href="404.html">404 Page</a>
-//                         <a className="collapse-item" href="blank.html">Blank Page</a>
-//                     </div>
-//                 </div>
-//             </li>
-
-//             <li className="nav-item">
-//                 <a className="nav-link" href="charts.html">
-//             <FontAwesomeIcon icon={faChartArea} />
-//                     <span>Charts</span></a>
-//             </li>
-
-//             <li className="nav-item">
-//                 <a className="nav-link" href="tables.html">
-//             <FontAwesomeIcon icon={faTable} />
-//                     <span>Tables</span></a>
-//             </li>
-
-//             <hr className="sidebar-divider d-none d-md-block"></hr>
-
-//             <div className="text-center d-none d-md-inline">
-//                 <button className="rounded-circle border-0" id="sidebarToggle"></button>
-//             </div>
-
-//             <div className="sidebar-card d-none d-lg-flex">
-//                 <img className="sidebar-card-illustration mb-2" src="img/undraw_rocket.svg" alt="..."></img>
-//                 <p className="text-center mb-2"><strong>SB Admin Pro</strong> is packed with premium features, components, and more!</p>
-//                 <a className="btn btn-success btn-sm" href="https://startbootstrap.com/theme/sb-admin-pro">Upgrade to Pro!</a>
-//             </div>
-
-//               </ul>
-//           </div>
-//   );
-// }
-
-// export default App;
-
-import React from 'react'
-import { useContext } from 'react';
-import { createContext , useState} from 'react'
-
-const profileContext=createContext();
+  // create a reference for the first input text box
+  const newNoteContentRef = useRef(null);
 
 
-function Profile()
-    {
-        const {profileName,setprofileName}=useContext(profileContext);
-        
-        let onChangeProfileNameHandler=(event)=>
-        setprofileName(event.target.value);
-        return(
-            <div>
-            <h2>Profile Name : {profileName}</h2>
-            <input type="text" value={profileName} onChange={onChangeProfileNameHandler}/>
-            </div>
-        )
+  // define the addNote method
+  let addNote = (event) => {
+    event.preventDefault();
+    
+    // create a new object
+    let noteObject = {
+      id: notes.length + 1,
+      content: newNoteContent,
+      important: newNoteImportant==='true',
     }
 
+    // add the new object to the notes state
+    setNotes(notes.concat(noteObject));
 
-function App() {
-    
-    const [profileName,setprofileName] =useState("");
-      return (
-        <div>
-            
-    <profileContext.Provider value = {{profileName , setprofileName}}>
-    <Profile />
-    </profileContext.Provider>
+    // console.log(noteObject);
 
+    // clear the input text box
+    setNewNoteContent('');
+    setNewNoteImportant('');
+    newNoteContentRef.current.focus();
+  }
+
+  let handleNoteChange = (event) => {
+    setNewNoteContent(event.target.value);
+  }
+
+  let handleSelectChange = (event) => {
+    setNewNoteImportant(event.target.value);
+  }
+
+  let handleStatusChange = (event) => {
+    setShowStatus(event.target.value);
+    // console.log(event.target.value);
+  }
+
+  let filterNotes = (notes, status) => {
+    switch (status) {
+      case 'all':
+        return notes;
+      case 'imp':
+        return notes.filter(note => note.important === true);
+      case 'nonimp':
+        return notes.filter(note => note.important === false);
+    }
+  }
+
+  const notesFiltered = filterNotes(notes, showStatus);
+
+  return (
+    <div>
+      <h1>Notes</h1>
+
+      <label>
+        <input type='radio' name='filter' value="all" checked={ showStatus === 'all' } onChange={handleStatusChange} />
+        Show All Notes
+      </label>
+
+      <label>
+        <input type='radio' name='filter' value="imp" checked={ showStatus === 'imp' } onChange={handleStatusChange} />
+        Show Important Notes
+      </label>
+
+      <label>
+        <input type='radio' name='filter' value="nonimp" checked={ showStatus === 'nonimp' } onChange={handleStatusChange} />
+        Show Non-Important Notes
+      </label>
+
+      <ul>
+        {notesFiltered.map(note => 
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
+
+      {/* add a simple form for adding notes */}
+      <form onSubmit={addNote}>
+        <input
+          value={newNoteContent}
+          onChange={handleNoteChange}
+          placeholder='type a note...' 
+          ref={newNoteContentRef}
+          /> <br /><br />
+        {/* <input
+          type='text'
+          placeholder='enter true or false'
+          value={newNoteImportant}
+          onChange={(e) => setNewNoteImportant(e.target.value)} /> */}
+        <label form='dropdownNoteImportant'>Is Important: </label>
+        <select
+          id='dropdownNoteImportant'
+          onChange={handleSelectChange}
+          value={newNoteImportant}>
+          <option>--Select--</option>
+          <option>true</option>
+          <option>false</option>
+        </select>
+        <br /><br />
+        <button type='submit'>Add New Note</button>
+      </form>
     </div>
   )
 }
+
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- src/index.js
+++ src/index.js
@@ -2,15 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 //import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-ReactDOM.createRoot(document.getElementById('root')).render(<App/>);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+
+    // const notes=[
+    //   {
+    //     id:1,
+    //     content:'Working with List',
+    //     important:'true'
+    //   },
+    //   {
+    //     id:2,
+    //     content:'Usage of Keys',
+    //     important:'false'
+    //   },
+    // {
+    //     id:3,
+    //     content:'Conditional Rendering',
+    //     important:'true'
+    //   },
+    //   {
+    //     id:4,
+    //     content:'Handling Events',
+    //     important:'true'
+    //   },
+    //   {
+    //     id:5,
+    //     content:'Props vs State',
+    //     important:'false'
+    //   },
+    // ];
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
